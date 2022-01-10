e57bb600fcf0cebcb1007f18cf6265deabde5c6a
htanivar <37423501+htanivar@users.noreply.github.com>
2022-01-10 13:58:34 +0530

    Angular (#9)
    * Anuglar Read me added
    
    * added .gitignore & ANGULAR_README.md
    
    * created angular project via angular cli
    
    * project-init
    
    * updated angular readme
    
    * generated Angular app
    
    * changed port to 5001
    
    * updated git documents
    
    * ng pi command added to expose it outside localhost
    
    * removed ng pi command
    
    * Linux.md added
    
    * exposed angular to any host
    
    * removed all the content from app.component.html
    
    * 32 angular (#5)
    
    * read the properties [first & last names] from app.component.ts file and displayed it in app.component.html
    
    * 34 angular (#8)
    
    * Project changed to Sale-Project
    
    * generated sales-person-list component
    
    * added the sales person list component to the main application template
    
    * new SalesPerson class is created by issuing `ng generate class sales-person-list/SalesPerson`
    
    * sales person class constructor is added
    
    * created sales person list
    
    * *ngFor is created to get the list of sales persons from component list

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Angular Learning';
+
+  title = 'sales-project';
+
   firstName = 'Harry';
   lastName = 'Potter';
 }
